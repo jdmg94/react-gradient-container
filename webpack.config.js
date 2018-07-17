2917e40a97c5d70ffd9867af3b1eb963a5885169
José Muñoz <jdmg93@gmail.com>
2018-07-16 20:34:44 -0600

    removed modules from source

--- webpack.config.js
+++ webpack.config.js
@@ -4,43 +4,46 @@
 * @Last modified by:   JoseMunoz
 * @Last modified time: 2018-07-16T13:11:52-06:00
 */
-require("babel-polyfill");
 const path = require('path');
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
-  template: path.join(__dirname, "examples/src/index.html"),
-  filename: "./index.html"
+  template: path.join(__dirname, 'examples/src/index.html'),
+  filename: './index.html'
 });
+
 module.exports = {
   optimization: {
     minimizer: [
       new UglifyJsPlugin()
     ]
   },
-  entry: ['babel-polyfill', path.join(__dirname, "examples/src/index.js")],
+  entry: [
+    path.join(__dirname, 'examples/src/index.js')
+  ],
   output: {
+    umdNamedDefine: true,
     libraryTarget: 'umd',
     library: 'react-gradient-container',
-    path: path.join(__dirname, "examples/dist"),
-    filename: "bundle.js"
+    path: path.join(__dirname, 'examples/dist'),
+    filename: 'bundle.js'
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
-        use: "babel-loader",
+        use: 'babel-loader',
         exclude: /node_modules/
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
   plugins: [htmlWebpackPlugin],
   resolve: {
-    extensions: [".js", ".jsx"]
+    extensions: ['.js', '.jsx']
   },
   devServer: {
     port: 3001
